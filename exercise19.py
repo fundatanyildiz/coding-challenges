59c79b410049f88fa0dff5125fc47a27ad18c077
Funda variş <funda.varis@gmail.com>
2023-12-05 12:40:17 +0100

    More efficient second solution is added for valid palindrome!

--- exercise19.py
+++ exercise19.py
@@ -4,18 +4,33 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        organized_s = [i.capitalize() for i in s if not i.isspace() and i.isalnum()]
-        print(organized_s)
-        if organized_s == organized_s[::-1]:
-            return True
-        else:
-            return False
+        # organized_s = [i.capitalize() for i in s if not i.isspace() and i.isalnum()]
+        # print(organized_s)
+        # if organized_s == organized_s[::-1]:
+        #     return True
+        # else:
+        #     return False
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if not s[left].isalnum():
+                print(s[left])
+                left += 1
+            elif not s[right].isalnum():
+                print(s[right])
+                right -= 1
+            elif s[left].lower() == s[right].lower():
+                left += 1
+                right -= 1
+            else:
+                return False
+        return True
 
 
 s1 = "A man, a plan, a canal: Panama"
-s2 = "race a car"
-s3 = "0P"
+# s2 = "race a car"
+# s3 = "0P"
 obj = Solution()
 print(obj.isPalindrome(s1))
-print(obj.isPalindrome(s2))
-print(obj.isPalindrome(s3))
+# print(obj.isPalindrome(s2))
+# print(obj.isPalindrome(s3))
